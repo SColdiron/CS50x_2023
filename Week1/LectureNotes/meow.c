700f241733a8442508a4a2fc2e75fb5dcbdad472
Sam Coldiron <72761310+SColdiron@users.noreply.github.com>
2023-02-05 00:45:59 -0600

    Create meow.c

---
+++ Week1/LectureNotes/meow.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int i = 0;
+    while (i < 3)
+    {
+        printf("meow\n");
+        i++;
+    }
+
+    for (int x = 0; x < 3; x++)
+    {
+        printf("meow\n");
+    }
+}
